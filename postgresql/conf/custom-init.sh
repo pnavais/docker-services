e0dd73882cdebfdf5e49916e0c24e1d1ac441dd6
Pablo Navais <pnavais@gmail.com>
2020-04-19 20:40:35 +0200

    Fixed hardcoded path to pg_hba.conf

--- postgresql/conf/custom-init.sh
+++ postgresql/conf/custom-init.sh
@@ -2,5 +2,5 @@
 
 DOCKER_BRIDGE_IP="172.17.0.1"
 
-sed -i "\$i # Allow all connections from docker bridge" /var/lib/postgresql/data/pg_hba.conf
-sed -i "\$i host\tall\t\tall\t\t$DOCKER_BRIDGE_IP/32\t\ttrust\n" /var/lib/postgresql/data/pg_hba.conf
+sed -i "\$i # Allow all connections from docker bridge" $PGDATA/pg_hba.conf
+sed -i "\$i host\tall\t\tall\t\t$DOCKER_BRIDGE_IP/32\t\ttrust\n" $PGDATA/pg_hba.conf
